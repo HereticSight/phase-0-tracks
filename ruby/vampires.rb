2e11627b4cd49b5da2477b1b8383bc9a2e8bc063
Kenny Phu <kphu@outlook.com>
2016-04-12 18:19:29 -0400

    add preliminary employee questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,26 @@
+#What is your name?
+
+puts "What is your name?"
+name = gets.chomp
+
+#How old are you? What year were you born?
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "In what year were you born?"
+birth_year = gets.chomp
+
+#Our company cafeteria serves garlic bread. 
+#Should we order some for you?
+
+puts "Our company cafeteria serves garlic bread. 
+How many servings would you like?"
+gbread_servings = gets.chomp
+
+#Would you like to enroll in the company's 
+#health insurance?
+
+puts "Would you like to enroll in the 
+company's health insurance?"
+health_ins = gets.chomp
