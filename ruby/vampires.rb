41889941e6ea9e8e5dfe7345a5c88e27024b7c22
Kenny Phu <kphu@outlook.com>
2016-04-12 20:52:05 -0400

    Add vampire checking conditions, and correct string errors

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,24 +3,47 @@
 puts "What is your name?"
 name = gets.chomp
 
+  if name == ""
+    name = nil
+  end
+
 #How old are you? What year were you born?
 
 puts "How old are you?"
 age = gets.chomp
 
+  if age == ""
+    age = nil
+  else
+    age = age.to_i
+  end
+
 current_year = Time.new.year
+
 puts "In what year were you born?"
 birth_year = gets.chomp
 
+  if birth_year == ""
+    birth_year = nil
+  else
+    birth_year = birth_year.to_i
+  end
 
-actual_age = current_year - birth_year
-
+  if birth_year == nil || age == nil
+    actual_age = nil
+  else
+    actual_age = current_year - birth_year
+  end
 #Our company cafeteria serves garlic bread. 
 #Should we order some for you?
 
 puts "Our company cafeteria serves garlic bread. 
-How many servings would you like?"
-gbread_servings = gets.chomp
+Should we order some for you?"
+gbread = gets.chomp
+  
+  if gbread == ""
+    gbread = "No"
+  end
 
 #Would you like to enroll in the company's 
 #health insurance?
@@ -29,33 +52,42 @@ puts "Would you like to enroll in the
 company's health insurance?"
 health_ins = gets.chomp
 
-#If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, 
+  if health_ins == ""
+    health_ins = "No"
+  end
+
+#Even if the employee is an amazing liar otherwise, 
+#anyone going by the name of “Drake Cula” or “Tu Fang” 
+#is clearly a vampire, because come on. 
+#In that case, you should print “Definitely a vampire.”
+
+if name == "Drake Cula" || name == "Tu Fang"
+  puts "“Definitely a vampire."
+
+#If the employee got their age right, and is 
+#willing to eat garlic bread or sign up for insurance, 
 #the result is “Probably not a vampire.”
 
-if (age == actual_age) && (gbread_servings >= 1 || health_ins == "Yes")
+elsif (age == actual_age) && (gbread == "Yes" || health_ins == "Yes")
   puts "Probably not a vampire."
 
-#If the employee got their age wrong, and hates garlic bread or waives insurance, the result is 
+#If the employee got their age wrong, and hates 
+#garlic bread or waives insurance, the result is 
 #“Probably a vampire.”
 
-elsif age != actual_age && (gbread_servings <= 1 || health_ins == "No")
+elsif age != actual_age && (gbread == "No" || health_ins == "No")
   puts "Probably a vampire."
 
-#If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is 
+#If the employee got their age wrong, hates garlic bread, 
+#and doesn’t want insurance, the result is 
 #“Almost certainly a vampire.”
 
-elsif (age != actual_age) && gbread_servings <= 1 && health_ins == "No"
+elsif age != actual_age && gbread == "No" && health_ins == "No"
   puts "Almost certainly a vampire."
-
-#Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” 
-#is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-
-elsif name = "Drake Cula" || name = "Tu Fang"
-  puts 
 
 #Otherwise, print “Results inconclusive.”
 
 else 
-  puts "Results inconclusive"
+  puts "Results inconclusive."
 
 end
