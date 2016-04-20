eff4a2eecd3eda87182da892bcf40cb1b1774c98
Kenny Phu <kphu@outlook.com>
2016-04-20 02:28:35 -0400

    Create Algorithm for alias maker

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+#Split name by word
+  #Swap first and last name in array
+    #For each item in the array:
+      #Split into letters
+      #Create vowel and consonant strings
+        #For each letter
+          #Determine if letter is:
+            #Vowel
+              #Advance letter one vowel forward
+            #Consonant
+              #Advance letter one consonant forward
+          #Combine each array back into names
+        #Combine each name back into a full name
+      
