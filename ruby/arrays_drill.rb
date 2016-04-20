1e2aafb76071269fc7124789b3c8d563313acb26
Kenny Phu <kphu@outlook.com>
2016-04-19 20:07:20 -0400

    add new object into array at index 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,20 +1,23 @@
 # initialize an array
 puts "Pandoras_Box =[]"
 Pandoras_Box = []
-
 # Print the array after initialization
 p Pandoras_Box
 
 # add 5 items to the array
 puts "Pandoras_Box << \"Expectation\" << \"Illness\" << \"Fear\" << \"Anxiety\"<< \"Hope\""
 Pandoras_Box << "Expectation" << "Illness" << "Fear" << "Anxiety"<< "Hope"
-
 # Print array after adding 5 items.
 p Pandoras_Box
 
 puts "Pandoras_Box.delete_at(2)"
 #delete the item at index 2
 Pandoras_Box.delete_at(2)
+# Print the array after deleting the object at index 2
+p Pandoras_Box
 
-# Print the array after deleting index 2
+# Insert new object at index 2
+puts "Pandoras_Box.insert(2,\"Loathing\")"
+Pandoras_Box.insert(2,'Loathing')
+# Prints the array with new object at index 2
 p Pandoras_Box
