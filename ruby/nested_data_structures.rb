580279295129adccd9d4ffa1afb8f028248fd439
Kenny Phu <kphu@outlook.com>
2016-04-20 00:05:52 -0400

    Add tea nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,31 @@
+Tea = {
+  green: [
+    'Sencha',
+    'Dragonwell',
+    'Gyokuro',
+  ],
+  black: [
+    'Darjeeling',
+    'Assam',
+    'Ying De Hong'
+  ],
+  white: [
+    'Silver Needle Tea',
+    'Yue Guang Bai',
+    'Jade Needle'
+  ],
+  oolong: [
+    'Wu Yi Rock',
+    'Dan Cong',
+    'Anxi'
+  ],
+  puerh: [
+    'Ripe Pu-erh',
+    'Raw Pu-erh',
+  ],
+  tisane: [
+    'Chai',
+    'Rooibos',
+    'Matte'
+  ]
+}
