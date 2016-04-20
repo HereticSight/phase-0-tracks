046070d96c6f757f4444e9d341a70a9a104c6946
Kenny Phu <kphu@outlook.com>
2016-04-20 10:03:45 -0400

    add map and each training

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,16 @@ def block_meth
 end
 
 block_meth { |x| puts "#{x.capitalize} must be a good power!"}
+
+big_cats = ["Bobcat", "Cougar", "Mountain Lion"]
+animals = {cat: "Bob", dog: "John", rat: "Pinky"}
+
+big_cats.each { |cat| puts "#{cat} is clearly bigger than a cat"}
+animals.each { |animal, name| puts "#{name} is a #{animal.to_s}"}
+
+puts "Before .map! call:"
+p big_cats
+big_cats.map! { |cat| cat = cat.swapcase }
+
+puts "After .map! call:"
+p big_cats
