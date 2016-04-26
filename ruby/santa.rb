447ebfb78dbac65bf8a542feb769b00b1762b803
Kenny Phu <kphu@outlook.com>
2016-04-26 15:40:18 -0400

    create santa class and initial methods

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+
+
+class Santa
+
+#Santa methods
+  def speak
+    puts "Santa says, \"Ho, ho, ho! Haaaappy holidays!\""
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "Santa says, \"That was a good #{cookie}!\""
+  end
+
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+  end
+end
