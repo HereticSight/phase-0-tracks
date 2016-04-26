9349a86e60f7736ce12271d4a36d25eef4f70dfc
Kenny Phu <kphu@outlook.com>
2016-04-26 15:42:16 -0400

    initialize a variety of santa instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -28,3 +28,14 @@ class Santa
   end
 
 end
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas
