18165acc956d985bd16d9c42111283c764c3a211
Kenny Phu <kphu@outlook.com>
2016-04-25 23:29:25 -0400

    add characteristics and behaviors for canis puppicus

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES --------------------
+# Canis Puppicus
+
+# CHARACTERISTICS ------------------
+# Height: 1.5 feet
+# Weight: 7.5 lbs
+# Color: White fur
+# Friendly: yes
+ 
+# BEHAVIOR -------------------
+# Lick
+# Bound
+# Sleep
+# Play Fight
