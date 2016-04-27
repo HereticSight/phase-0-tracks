558ed4c80c5ed613326acf74a32118966a6001b9
Kenny Phu <kphu@outlook.com>
2016-04-27 19:42:01 -0400

    teach puppy tricks and add driver code to test

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times {puts "Woof!"}
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def dog_years(human_years)
+    dog_age= human_years*7
+  end
+  
+  def play_dead
+    puts "*playing dead*"
+  end
 end
 
+
+spot= Puppy.new
+spot.fetch("ball")
+spot.speak(3)
+spot.roll_over
+p spot.dog_years(2)
+spot.play_dead
