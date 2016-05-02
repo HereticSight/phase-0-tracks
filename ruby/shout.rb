a8c349d5ff9b0c1efa3fc3e352076463aa80a240
Kenny Phu <kphu@outlook.com>
2016-05-02 04:01:33 -0400

    add angry and happy yells for students and teachers

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,37 @@
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  # def self.yell_angrily(words)
+  #   words + "!!!" + " :("
+  # end
+
+  # def self.yelling_happily(words)
+  #   words + "!!!" + " :)"
+  # end
+
+  def yell_angrily(words)
+    words + "!!!" + " >:["
   end
 
-  def self.yelling_happily(words)
-    words + "!!!" + " :)"
+  def yell_happily(words)
+    words + "!!!" + " :D"
   end
 end
 
-class Moop
+class Teacher
+  include Shout
 end
 
-puts Shout.yell_angrily("Not again")
-puts Shout.yelling_happily("Hello")
+class Student
+  include Shout
+end
+
+teacher = Teacher.new
+puts teacher.yell_angrily("The class average is barely passing")
+puts teacher.yell_happily("I've created some remedial work for everyone")
+student = Student.new
+puts student.yell_angrily("More homework?")
+puts student.yell_happily("It's Spring Break")
+
+
+# puts Shout.yell_angrily("Not again")
+# puts Shout.yelling_happily("Hello")
